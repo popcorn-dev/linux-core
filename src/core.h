ec27d4c8209e11debae046225c809f7cccc4e08a
gunnerDgd <ifstudiomail@naver.com>
2024-06-17 12:47:32 +0900

    [Progress] /src/core for Core Library Initialization

---
+++ src/core.h
@@ -0,0 +1,14 @@
+#ifndef __CORE_H__
+#define __CORE_H__
+
+#include <core.h>
+
+#include "class.h"
+
+extern po_obj_trait *core_t;
+struct               core  {
+    po_obj   head;
+    po_class class;
+};
+
+#endif
